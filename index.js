06e93138add7baf5ff84fcc461e8f644b06f5e73
shuntakagi <s.takagi.910@icloud.com>
2017-06-05 00:49:00 +0900

    Create new file deredtry

---
+++ index.js
@@ -0,0 +1,62 @@
+$(function(){
+
+   $('.column-box').hide();
+   $('.top').fadeIn();
+
+
+   $('#login-show').click(
+   function(){
+     $('#login-modal').fadeIn();
+   });
+
+   $('.signup-show').click(
+   function(){
+     $('#signup-modal').fadeIn();
+     $('#login-modal').hide();
+   });
+
+   $('.close-modal').click(function(){
+    $('#signup-modal').fadeOut();
+    $('#login-modal').fadeOut();
+  });
+
+
+
+	 $('.column-image').hover(
+         function(){
+           $(this).addClass('column-hover');
+           $(this).children('.zoom-black').fadeIn(300);
+         },
+         function(){
+           $(this).removeClass('column-hover');
+           $(this).children('.zoom-black').fadeOut(300);
+         });
+
+
+	 $('.filter-item').click(
+        function(){
+          $('.filter-item').removeClass('active');
+          $(this).addClass('active');
+          var category = $(this).attr('id');
+          if(category== 'all'){
+            $('.column-box').fadeIn();
+          }else if(category=='100'){
+            $('.column-box').hide();
+            $('.100').fadeIn();
+          }else if(category=='200'){
+            $('.column-box').hide();
+            $('.200').fadeIn();
+          }else if(category=='300'){
+            $('.column-box').hide();
+            $('.300').fadeIn();
+          }else if(category=='400'){
+            $('.column-box').hide();
+            $('.400').fadeIn();
+          }else if(category=='500'){
+            $('.column-box').hide();
+            $('.500').fadeIn();
+          };
+          
+          
+        });
+});
